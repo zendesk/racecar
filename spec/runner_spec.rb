e63d300965241fa47795610fe082d1aa5d93af7f
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2017-05-19 13:46:37 +0200

    Improve the Runner spec

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -5,10 +5,19 @@ class TestConsumer < Racecar::Consumer
 
   def initialize
     @messages = []
+    @processor_queue = []
+  end
+
+  def on_message(&block)
+    @processor_queue << block
+    self
   end
 
   def process(message)
     @messages << message
+
+    processor = @processor_queue.shift || proc {} 
+    processor.call(message)
   end
 end
 
@@ -18,15 +27,32 @@ class FakeConsumer
   end
 
   def each_message(*options, &block)
-    @kafka.messages.each(&block)
+    @kafka.messages.each do |message|
+      begin
+        block.call(message)
+      rescue StandardError => e
+        raise Kafka::ProcessingError.new(message.topic, message.partition, message.offset)
+      end
+    end
+  end
+
+  def pause(topic, partition, timeout:)
+    @kafka.paused_partitions[topic] ||= {}
+    @kafka.paused_partitions[topic][partition] = true
   end
 end
 
 class FakeKafka
-  attr_reader :messages
+  attr_reader :messages, :paused_partitions
 
   def initialize(*options)
     @messages = []
+    @paused_partitions = {}
+  end
+
+  def paused?(topic, partition)
+    @paused_partitions[topic] ||= {}
+    !!@paused_partitions[topic][partition]
   end
 
   def deliver_message(value, topic:)
@@ -49,18 +75,47 @@ describe Racecar::Runner do
   let(:logger) { Logger.new(StringIO.new) }
   let(:processor) { TestConsumer.new }
   let(:kafka) { FakeKafka.new }
+  let(:runner) { Racecar::Runner.new(processor, config: config, logger: logger) }
 
   before do
     allow(Kafka).to receive(:new) { kafka }
   end
 
   it "processes messages with the specified consumer class" do
-    runner = Racecar::Runner.new(processor, config: config, logger: logger)
-
     kafka.deliver_message("hello world", topic: "greetings")
 
     runner.run
 
     expect(processor.messages.map(&:value)).to eq ["hello world"]
+  end
+
+  context "when the processing code raises an exception" do
+    it "pauses the partition if `pause_timeout` is > 0" do
+      config.pause_timeout = 10
+
+      processor
+        .on_message {|message| raise "OMG COOKIES!" }
+        .on_message {}
+
+      kafka.deliver_message("hello world", topic: "greetings")
+
+      runner.run
+
+      expect(kafka.paused?("greetings", 0)).to eq true
+    end
+
+    it "does not pause the partition if `pause_timeout` is 0" do
+      config.pause_timeout = 0
+
+      processor
+        .on_message {|message| raise "OMG COOKIES!" }
+        .on_message {}
+
+      kafka.deliver_message("hello world", topic: "greetings")
+
+      runner.run
+
+      expect(kafka.paused?("greetings", 0)).to eq false
+    end
   end
 end
