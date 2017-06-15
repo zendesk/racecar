4695ada29742f2c11f1ff48ffb233b60f74bcbef
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2017-06-14 22:25:48 -0700

    Add a `consumer` generator

---
+++ lib/generators/racecar/consumer_generator.rb
@@ -0,0 +1,11 @@
+module Racecar
+  module Generators
+    class ConsumerGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path("../../templates", __FILE__)
+
+      def create_consumer_file
+        template "consumer.rb.erb", "app/consumers/#{file_name}_consumer.rb"
+      end
+    end
+  end
+end
