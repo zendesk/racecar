8f39dd4271c4cb355ce799230960b650e5432886
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2017-06-21 16:03:51 +0200

    Allow configuring the max number of bytes to fetch
    This is configured on a per topic basis and applies to the number of
    bytes fetched from a single partition.

--- lib/racecar/consumer.rb
+++ lib/racecar/consumer.rb
@@ -1,6 +1,6 @@
 module Racecar
   class Consumer
-    Subscription = Struct.new(:topic, :start_from_beginning)
+    Subscription = Struct.new(:topic, :start_from_beginning, :max_bytes_per_partition)
 
     class << self
       attr_accessor :max_wait_time
@@ -11,9 +11,14 @@ module Racecar
       end
 
       # Adds one or more topic subscriptions.
-      def subscribes_to(*topics, start_from_beginning: true)
+      #
+      # start_from_beginning    - whether to start from the beginning or the end of each
+      #                           partition.
+      # max_bytes_per_partition - the maximum number of bytes to fetch from each partition
+      #                           at a time.
+      def subscribes_to(*topics, start_from_beginning: true, max_bytes_per_partition: 1048576)
         topics.each do |topic|
-          subscriptions << Subscription.new(topic, start_from_beginning)
+          subscriptions << Subscription.new(topic, start_from_beginning, max_bytes_per_partition)
         end
       end
     end
