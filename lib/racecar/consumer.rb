e9764c1b1e05870fc0ecaf49851d120ddd128a31
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-05-18 14:17:54 +0200

    Document `Consumer.subscribes_to`

--- lib/racecar/consumer.rb
+++ lib/racecar/consumer.rb
@@ -13,10 +13,14 @@ module Racecar
 
       # Adds one or more topic subscriptions.
       #
-      # start_from_beginning    - whether to start from the beginning or the end of each
-      #                           partition.
-      # max_bytes_per_partition - the maximum number of bytes to fetch from each partition
-      #                           at a time.
+      # Can be called multiple times in order to subscribe to more topics.
+      #
+      # @param topics [String] one or more topics to subscribe to.
+      # @param start_from_beginning [Boolean] whether to start from the beginning or the end
+      #   of each partition.
+      # @param max_bytes_per_partition [Integer] the maximum number of bytes to fetch from
+      #   each partition at a time.
+      # @return [nil]
       def subscribes_to(*topics, start_from_beginning: true, max_bytes_per_partition: 1048576)
         topics.each do |topic|
           subscriptions << Subscription.new(topic, start_from_beginning, max_bytes_per_partition)
