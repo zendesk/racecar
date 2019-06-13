83c8ef176ad3147d1fcff5e914bd71b98dd14796
Nadav Karpenkop <nadavk72@gmail.com>
2019-06-14 01:34:54 +0300

    fix load configuration file problem

--- lib/racecar/cli.rb
+++ lib/racecar/cli.rb
@@ -26,7 +26,7 @@ module Racecar
       RailsConfigFileLoader.load!
 
       if File.exist?("config/racecar.rb")
-        require "racecar/config"
+        require "./config/racecar"
       end
 
       # Find the consumer class by name.
--- lib/racecar/version.rb
+++ lib/racecar/version.rb
@@ -1,3 +1,3 @@
 module Racecar
-  VERSION = "0.5.3.beta2"
+  VERSION = "0.5.4.beta2"
 end
