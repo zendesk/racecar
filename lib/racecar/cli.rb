ca9f8898963110ad04efd0512874608ab0b4d9c9
Nadav Karpenkop <nadavk72@gmail.com>
2019-06-14 01:01:22 +0300

    try

--- lib/racecar/cli.rb
+++ lib/racecar/cli.rb
@@ -26,7 +26,7 @@ module Racecar
       RailsConfigFileLoader.load!
 
       if File.exist?("config/racecar.rb")
-        require "config/racecar"
+        require "racecar/config"
       end
 
       # Find the consumer class by name.
