ad3b076d4c34c55ac7e114e798978cd64ac9d916
Daniel Schierbeck <daniel@dasch.io>
2020-08-26 14:46:42 +0200

    Expand and improve the integration specs

--- spec/integration/consumer_spec.rb
+++ spec/integration/consumer_spec.rb
@@ -4,63 +4,112 @@ require "securerandom"
 require "racecar/cli"
 require "racecar/ctl"
 
-class EchoConsumer < Racecar::Consumer
-  subscribes_to "input"
+def generate_token
+  SecureRandom.hex(8)
+end
 
-  self.group_id = "test-consumer-#{SecureRandom.hex(8)}"
+INPUT_TOPIC = "input-#{generate_token}"
+OUTPUT_TOPIC = "output-#{generate_token}"
+
+class EchoConsumer < Racecar::Consumer
+  subscribes_to INPUT_TOPIC
+
+  self.group_id = "echo-consumer-#{generate_token}"
 
   def process(message)
     produce message.value, key: message.key, topic: "output"
   end
 end
 
-module IntegrationSupport
-  INCOMING_MESSAGES = []
+class ResultsConsumer < Racecar::Consumer
+  subscribes_to OUTPUT_TOPIC
 
-  CONSUMER = Thread.new do
-    consumer = Rdkafka::Config.new({
-      "bootstrap.servers": Racecar.config.brokers.join(","),
-      "client.id":         Racecar.config.client_id,
-      "group.id":          "racecar-tests",
-    }.merge(Racecar.config.rdkafka_consumer)).consumer
+  self.group_id = "results-consumer-#{generate_token}"
 
-    consumer.subscribe("output")
+  MESSAGES = []
 
-    consumer.each do |message|
-      puts "Received message #{message}"
-      INCOMING_MESSAGES << message
-    end
-  end
-
-  def self.incoming_messages
-    INCOMING_MESSAGES
+  def process(message)
+    puts "Received token #{message.value} in #{message.inspect}"
+    MESSAGES << message
   end
 end
 
-RSpec.describe "running a Racecar consumer" do
-  it "works" do
-    worker = Thread.new do
-      cli = Racecar::Cli.new(["EchoConsumer"])
-      cli.run
+RSpec.context "Integrating with a real Kafka cluster" do
+  before :all do
+    Thread.new do
+      Racecar::Cli.main(["ResultsConsumer"])
     end
+  end
 
-    ctl = Racecar::Ctl.main %w(
-      produce -t input -v hello -k greetings
-    )
+  after do
+    ResultsConsumer::MESSAGES.clear
+  end
 
+  describe "Single-consumer groups" do
+    it "can consume and produce messages" do
+      token = generate_token
+
+      worker = Thread.new do
+        Racecar::Cli.main(["EchoConsumer"])
+      end
+
+      produce_token(token)
+
+      message = wait_for_token(token)
+
+      expect(message).not_to be_nil
+      expect(message.value).to eq token
+      expect(message.key).to eq "greetings"
+    end
+  end
+
+  describe "Multi-consumer groups" do
+    it "can consume and produce messages" do
+      token = generate_token
+
+      worker1 = Thread.new do
+        Racecar::Cli.main(["EchoConsumer"])
+      end
+
+      worker2 = Thread.new do
+        Racecar::Cli.main(["EchoConsumer"])
+      end
+
+      worker3 = Thread.new do
+        Racecar::Cli.main(["EchoConsumer"])
+      end
+
+      Racecar::Ctl.main %W(
+        produce -t #{INPUT_TOPIC} -v #{token} -k greetings
+      )
+
+      message = wait_for_token(token)
+
+      expect(message).not_to be_nil
+      expect(message.value).to eq token
+      expect(message.key).to eq "greetings"
+    end
+  end
+
+  def wait_for_token(token)
     message = nil
     attempt = 1
 
-    while message.nil? && attempt <= 10
-      puts "Waiting for message..."
+    while message.nil? && attempt <= 3
+      puts "Waiting for token #{token}..."
       sleep 2 ** attempt
-      message = IntegrationSupport.incoming_messages.last
+      message = ResultsConsumer::MESSAGES.find {|m| m.value == token }
       attempt += 1
     end
 
-    expect(message).not_to be_nil
-    expect(message.topic).to eq "output"
-    expect(message.payload).to eq "hello"
-    expect(message.key).to eq "greetings"
+    message
+  end
+
+  def produce_token(token)
+    puts "Writing token #{token} to #{INPUT_TOPIC}"
+
+    Racecar::Ctl.main %W(
+      produce -t #{INPUT_TOPIC} -v #{token} -k greetings
+    )
   end
 end
