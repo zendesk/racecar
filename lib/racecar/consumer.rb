0114e4ad79f83f7db73e646fe5139e073650b8f5
Daniel Schierbeck <daniel@dasch.io>
2020-08-31 15:36:08 +0200

    Allow `key:` to be nil in `#produce`

--- lib/racecar/consumer.rb
+++ lib/racecar/consumer.rb
@@ -54,7 +54,7 @@ module Racecar
     protected
 
     # https://github.com/appsignal/rdkafka-ruby#producing-messages
-    def produce(payload, topic:, key:, partition_key: nil, headers: nil, create_time: nil)
+    def produce(payload, topic:, key: nil, partition_key: nil, headers: nil, create_time: nil)
       @delivery_handles ||= []
       message_size = payload.respond_to?(:bytesize) ? payload.bytesize : 0
       instrumentation_payload = {
