de3df5ffe5a5ec879e33502704a0043228bc79ec
Colin Kelley <colindkelley@gmail.com>
2020-09-18 09:28:54 -0700

    issue #186: rescue exceptions in exe/racecar; add exe/racecar_spec

---
+++ spec/exe/racecar_spec.rb
@@ -0,0 +1,53 @@
+
+module Racecar
+  class << self
+    attr_accessor :exit_code
+
+    def exit(code = 0)
+      @exit_code = code
+    end
+  end
+end
+
+RSpec.describe "exe/racecar" do
+  before do
+    Racecar.exit_code = -1
+  end
+
+  context "when exception raised during startup" do
+    before do
+      @orig_argv = ::ARGV
+      Object.send(:remove_const, 'ARGV')
+      ::ARGV = ['--require', 'missing']
+    end
+
+    after do
+      Object.send(:remove_const, 'ARGV')
+      ::ARGV = @orig_argv
+    end
+
+    it "displays exception and exits with status 1" do
+      expect($stderr).to receive(:puts).with(/=> Crashed: LoadError: cannot load such file -- missing\n.*cli\.rb/)
+
+      load "./exe/racecar"
+
+      expect(Racecar.exit_code).to eq(1)
+    end
+  end
+
+  context "when SystemExit raised during startup" do
+    it "displays nothing and re-raises" do
+      expect($stderr).to_not receive(:puts)
+
+      SystemExit
+
+      expect(Racecar::Cli).to receive(:main).with(anything) { raise SystemExit }
+
+      expect do
+        load "./exe/racecar"
+      end.to raise_exception(SystemExit)
+
+      expect(Racecar.exit_code).to eq(-1)
+    end
+  end
+end
