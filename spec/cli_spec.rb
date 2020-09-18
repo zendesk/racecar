f6afc3b0bc1f966f9ec84b3c47be0bc30fab5882
Colin Kelley <colindkelley@gmail.com>
2020-09-17 18:15:42 -0700

    issue #186: remove 2 special case rescues; flesh out cli_spec to test raise and $stderr.puts

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -6,34 +6,40 @@ RSpec.describe Racecar::Cli do
     Racecar.config = nil
   end
 
-  it "fails if no consumer class is specified" do
+  it "raise exception if no consumer class is specified" do
     expect { Racecar::Cli.main([]) }.to raise_exception(Racecar::Error, "no consumer specified")
   end
 
-  it "doesn't start Rails if --without-rails option is specified" do
-    args = ["--require", "./examples/cat_consumer.rb", "CatConsumer", "--without-rails"]
+  context "displaying startup message" do
+    before do
+      expect($stderr).to receive(:puts).with(/Starting Racecar consumer CatConsumer/)
+      expect($stderr).to receive(:puts).with(/Wro+m!/)
+      expect($stderr).to receive(:puts).with(/Ctrl-C to shutdown consumer/)
+    end
 
-    allow(Racecar).to receive(:run)
-    expect(Racecar::RailsConfigFileLoader).not_to receive(:load!)
+    it "doesn't start Rails if --without-rails option is specified" do
+      args = ["--require", "./examples/cat_consumer.rb", "CatConsumer", "--without-rails"]
 
-    Racecar::Cli.main(args)
-  end
+      allow(Racecar).to receive(:run)
+      expect(Racecar::RailsConfigFileLoader).not_to receive(:load!)
 
-  it "starts Rails if the --without-rails option is omitted" do
-    args = ["--require", "./examples/cat_consumer.rb", "CatConsumer"]
-
-    allow(Racecar).to receive(:run)
-    expect(Racecar::RailsConfigFileLoader).to receive(:load!)
-
-    Racecar::Cli.main(args)
-  end
-
-  it "outputs a helpful message and exits if the required library fails to load" do
-    args = ["--require", "./spec/support/bad_library.rb", "BadLibrary::BadConsumer"]
-    expected_output = "=> ./spec/support/bad_library.rb failed to load: BadLibrary failed to load\n"
-
-    expect {
       Racecar::Cli.main(args)
-    }.to raise_error(SystemExit).and output(expected_output).to_stderr
+    end
+
+    it "starts Rails if the --without-rails option is omitted" do
+      args = ["--require", "./examples/cat_consumer.rb", "CatConsumer"]
+
+      allow(Racecar).to receive(:run)
+      expect(Racecar::RailsConfigFileLoader).to receive(:load!)
+
+      Racecar::Cli.main(args)
+    end
+  end
+
+  it "raises an exception if the required library fails to load" do
+    args = ["--require", "./spec/support/bad_library.rb", "BadLibrary::BadConsumer"]
+    expect($stderr).to_not receive(:puts)
+
+    expect { Racecar::Cli.main(args) }.to raise_error(StandardError, "BadLibrary failed to load")
   end
 end
