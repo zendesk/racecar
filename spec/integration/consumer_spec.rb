dd764840d96b3a1c27f57dd8dbf9bef343cce4c3
Alex Blair <alex.blair@fundingcircle.com>
2021-02-23 09:53:40 +0000

    Rework integration tests in preparation for additional test cases
    - Extracts tasks like waiting for a message to arrive into an
      integration helper module
    - Covers a couple of failure scenarios
    - Uses different topics for each spec, and for each suite run
    - Cleans up the topics at the end. This should mean that a developer
      running the suite locally doesn't end up with hundreds of topics
      hanging around!
    - We also allow the graceful shutdown to run after the spec run

--- spec/integration/consumer_spec.rb
+++ spec/integration/consumer_spec.rb
@@ -4,63 +4,94 @@ require "securerandom"
 require "racecar/cli"
 require "racecar/ctl"
 
-class EchoConsumer < Racecar::Consumer
-  subscribes_to "input"
-
-  self.group_id = "test-consumer-#{SecureRandom.hex(8)}"
-
-  def process(message)
-    produce message.value, key: message.key, topic: "output"
-  end
+class NoSubsConsumer < Racecar::Consumer
+  def process(message); end
 end
 
-module IntegrationSupport
-  INCOMING_MESSAGES = []
-
-  CONSUMER = Thread.new do
-    consumer = Rdkafka::Config.new({
-      "bootstrap.servers": Racecar.config.brokers.join(","),
-      "client.id":         Racecar.config.client_id,
-      "group.id":          "racecar-tests",
-    }.merge(Racecar.config.rdkafka_consumer)).consumer
-
-    consumer.subscribe("output")
-
-    consumer.each do |message|
-      puts "Received message #{message}"
-      INCOMING_MESSAGES << message
-    end
-  end
-
-  def self.incoming_messages
-    INCOMING_MESSAGES
-  end
+class NoProcessConsumer < Racecar::Consumer
+  subscribes_to "some-topic"
 end
 
-RSpec.describe "running a Racecar consumer" do
-  it "works" do
-    worker = Thread.new do
-      cli = Racecar::Cli.new(["EchoConsumer"])
-      cli.run
+RSpec.describe "running a Racecar consumer", type: :integration do
+  context "when an error occurs trying to start the runner" do
+    context "when there are no subscriptions" do
+      it "raises an exception" do
+        expect do
+          Racecar::Cli.new(["NoSubsConsumer"]).run
+        end.to raise_error(ArgumentError)
+      end
     end
 
-    ctl = Racecar::Ctl.main %w(
-      produce -t input -v hello -k greetings
-    )
+    context "when there is no process method" do
+      it "raises an exception" do
+        expect do
+          Racecar::Cli.new(["NoProcessConsumer"]).run
+        end.to raise_error(NotImplementedError)
+      end
+    end
+  end
 
-    message = nil
-    attempt = 1
+  context "when the runner starts successfully" do
+    let(:input_topic) { generate_input_topic_name }
+    let(:output_topic) { generate_output_topic_name }
+    let(:mock_echo_consumer_class) do
+      Class.new(Racecar::Consumer) do
+        class << self
+          attr_accessor :output_topic
+        end
 
-    while message.nil? && attempt <= 10
-      puts "Waiting for message..."
-      sleep 2 ** attempt
-      message = IntegrationSupport.incoming_messages.last
-      attempt += 1
+        def process(message)
+          produce message.value, key: message.key, topic: self.class.output_topic
+          deliver!
+        end
+      end
     end
 
-    expect(message).not_to be_nil
-    expect(message.topic).to eq "output"
-    expect(message.payload).to eq "hello"
-    expect(message.key).to eq "greetings"
+    let(:run_in_background!) do
+      Thread.new do
+        Thread.current.abort_on_exception = true
+        Racecar::Cli.new([consumer_class.name.to_s]).run
+      end
+    end
+
+    before do
+      create_topic(topic: input_topic, partitions: topic_partitions)
+
+      consumer_class.subscribes_to(input_topic)
+      consumer_class.output_topic = output_topic
+
+      rdkafka_consumer.subscribe(output_topic)
+
+      publish_messages!(input_topic, input_messages)
+
+      run_in_background!
+    end
+
+    after { Process.kill("INT", 0) }
+
+    after(:all) { delete_all_test_topics }
+
+    context "for a single threaded consumer" do
+      let(:consumer_class) do
+        class EchoConsumer1 < mock_echo_consumer_class
+          self.group_id = "echo-consumer-1"
+        end
+        EchoConsumer1
+      end
+      let(:input_messages) { [{ payload: "hello", key: "greetings", partition: nil }] }
+      let(:topic_partitions) { 1 }
+      let(:concurrency) { 1 }
+
+      it "can consume and publish a message" do
+        wait_for_messages(topic: input_topic, expected_message_count: 1)
+
+        message = incoming_messages.first
+
+        expect(message).not_to be_nil
+        expect(message.topic).to eq output_topic
+        expect(message.payload).to eq "hello"
+        expect(message.key).to eq "greetings"
+      end
+    end
   end
 end
