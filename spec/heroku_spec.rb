d268f057f6a7b2c7b96411080492162e98cd6db2
Alan Joseph <alanjsph@live.com>
2021-05-19 12:50:31 +0530

    Remove whitespaces

--- spec/heroku_spec.rb
+++ spec/heroku_spec.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# require "racecar/heroku"
 require "racecar/config"
 
 RSpec.describe "Racecar::Heroku" do
@@ -11,7 +10,7 @@ RSpec.describe "Racecar::Heroku" do
       context 'when Racecar::Heroku.kafka_name= is not called (default behavior)' do
         it 'doesn\'t do anything' do
           expect($stderr).not_to receive(:puts).with("Error: ENV KAFKA_CLIENT_CERT_KEY is not set")
-          
+
           with_env('KAFKA_URL', 'valid-url') do
             with_env('KAFKA_TRUSTED_CERT', 'valid trusted cert') do
               with_env('KAFKA_CLIENT_CERT', 'valid client cert') do
@@ -27,10 +26,10 @@ RSpec.describe "Racecar::Heroku" do
 
       context 'when Racecar::Heroku.kafka_name= is called (overriding behavior)' do
         it 'logs the error and exits with status 1' do
-        
+
           expect($stderr).to receive(:puts).with("=> Loading configuration from Heroku Kafka ENVs")
           expect($stderr).to receive(:puts).with("Error: ENV KAFKA2_CLIENT_CERT_KEY is not set")
-  
+
           with_env('KAFKA2_URL', 'valid-url') do
             with_env('KAFKA2_TRUSTED_CERT', 'valid trusted cert') do
               with_env('KAFKA2_CLIENT_CERT', 'valid client cert') do
@@ -60,35 +59,35 @@ RSpec.describe "Racecar::Heroku" do
             end
           end
         end
-  
+
         it 'sets the config.security_protocol to :ssl' do
           expect(Racecar.config.security_protocol).to eq :ssl
         end
-  
+
         it 'sets the config.brokers to KAFKA_URL' do
           expect(Racecar.config.brokers).to eq ['url1', 'url2']
         end
-  
+
         it 'sets the config.ssl_ca_location to a temporary file with contents of KAFKA_TRUSTED_CERT' do
           file_extention = Racecar.config.ssl_ca_location.split('.').last
           file_contents = File.read(Racecar.config.ssl_ca_location)
-  
+
           expect(file_extention).to eq 'pem'
           expect(file_contents).to eq 'valid trusted cert'
         end
-  
+
         it 'sets the config.ssl_certificate_location to a temporary file with contents of KAFKA_CLIENT_CERT' do
           file_extention = Racecar.config.ssl_certificate_location.split('.').last
           file_contents = File.read(Racecar.config.ssl_certificate_location)
-  
+
           expect(file_extention).to eq 'pem'
           expect(file_contents).to eq 'valid client cert'
         end
-        
+
         it 'sets the config.ssl_key_location to a temporary file with contents of KAFKA_CLIENT_CERT_KEY' do
           file_extention = Racecar.config.ssl_key_location.split('.').last
           file_contents = File.read(Racecar.config.ssl_key_location)
-  
+
           expect(file_extention).to eq 'pem'
           expect(file_contents).to eq 'valid client cert key'
         end
@@ -108,35 +107,35 @@ RSpec.describe "Racecar::Heroku" do
             end
           end
         end
-  
+
         it 'sets the config.security_protocol to :ssl' do
           expect(Racecar.config.security_protocol).to eq :ssl
         end
-  
+
         it 'sets the config.brokers to PURPLE_KAFKA_URL' do
           expect(Racecar.config.brokers).to eq ['purple-url1', 'purple-url2']
         end
-  
+
         it 'sets the config.ssl_ca_location to a temporary file with contents of PURPLE_KAFKA_TRUSTED_CERT' do
           file_extention = Racecar.config.ssl_ca_location.split('.').last
           file_contents = File.read(Racecar.config.ssl_ca_location)
-  
+
           expect(file_extention).to eq 'pem'
           expect(file_contents).to eq 'valid purple trusted cert'
         end
-  
+
         it 'sets the config.ssl_certificate_location to a temporary file with contents of PURPLE_KAFKA_CLIENT_CERT' do
           file_extention = Racecar.config.ssl_certificate_location.split('.').last
           file_contents = File.read(Racecar.config.ssl_certificate_location)
-  
+
           expect(file_extention).to eq 'pem'
           expect(file_contents).to eq 'valid purple client cert'
         end
-        
+
         it 'sets the config.ssl_key_location to a temporary file with contents of PURPLE_KAFKA_CLIENT_CERT_KEY' do
           file_extention = Racecar.config.ssl_key_location.split('.').last
           file_contents = File.read(Racecar.config.ssl_key_location)
-  
+
           expect(file_extention).to eq 'pem'
           expect(file_contents).to eq 'valid purple client cert key'
         end
