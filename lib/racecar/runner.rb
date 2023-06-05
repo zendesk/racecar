a3df6f27dca0b6304bd9f271d0956d1f5c0ede2a
Rajesh Sharma <rajesh.sharma@zendesk.com>
2023-06-05 11:35:11 +0200

    Add same delivery report handling for both producers

--- lib/racecar/runner.rb
+++ lib/racecar/runner.rb
@@ -162,11 +162,20 @@ module Racecar
 
     def delivery_callback
       ->(delivery_report) do
-        payload = {
-          offset: delivery_report.offset,
-          partition: delivery_report.partition
-        }
-        @instrumenter.instrument("acknowledged_message", payload)
+        if delivery_report.error.to_i.positive?
+          instrumentation_payload = {
+            topic: delivery_report.topic,
+            partition: delivery_report.partition,
+            exception: delivery_report.error
+          }
+          @instrumenter.instrument("produce_error", instrumentation_payload)
+        else
+          payload = {
+            offset: delivery_report.offset,
+            partition: delivery_report.partition
+          }
+          @instrumenter.instrument("acknowledged_message", payload)
+        end
       end
     end
 
