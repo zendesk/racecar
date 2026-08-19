f30ce458bfee8bd80bc1e318c4e20657d84b2216
pawurb <contact@pawelurbanek.com>
2026-08-19 15:11:10 +0200

    Add single_consumer config

--- spec/rebalance_listener_spec.rb
+++ spec/rebalance_listener_spec.rb
@@ -31,6 +31,41 @@ RSpec.describe Racecar::RebalanceListener do
     end
   end
 
+  describe "single-topic accessors on a multi-topic event" do
+    let(:partitions_by_topic_hash) do
+      {
+        "topic_a" => [double(:partition, partition: 0)],
+        "topic_b" => [double(:partition, partition: 1)],
+      }
+    end
+
+    it "raises from topic_name and partition_numbers instead of pairing them inconsistently" do
+      listener.on_partitions_assigned(rdkafka_topic_partition_list)
+
+      expect(consumer_class).to have_received(:on_partitions_assigned) do |rebalance_event|
+        expect { rebalance_event.topic_name }.to raise_error(Racecar::Error, /topic_partition_numbers/)
+        expect { rebalance_event.partition_numbers }.to raise_error(Racecar::Error, /topic_partition_numbers/)
+      end
+    end
+  end
+
+  describe "#topic_partition_numbers" do
+    let(:partitions_by_topic_hash) do
+      {
+        "topic_a" => [double(:partition, partition: 0), double(:partition, partition: 1)],
+        "topic_b" => [double(:partition, partition: 2)],
+      }
+    end
+
+    it "exposes the partitions of every topic in the event" do
+      listener.on_partitions_assigned(rdkafka_topic_partition_list)
+
+      expect(consumer_class).to have_received(:on_partitions_assigned) do |rebalance_event|
+        expect(rebalance_event.topic_partition_numbers).to eq("topic_a" => [0, 1], "topic_b" => [2])
+      end
+    end
+  end
+
   describe "#on_partitions_revoked" do
     it "calls the consumer class' callback passing the rebalance event" do
       listener.on_partitions_revoked(rdkafka_topic_partition_list)
